ffc0c4c5cbd5b8e359090e1ce3a38385b923c4a7
Маркин Даниил Романович <danya_markin_2000@mail.ru>
2024-09-17 17:49:43 +0300

    замена базы данных с локальной на удаленную

--- init.sql
+++ init.sql
@@ -1,50 +1,28 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+DO $CREATION$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'tender_status_enum') THEN
+        CREATE TYPE tender_status_enum AS ENUM ('Created', 'Published', 'Closed');
+    END IF;
 
-CREATE TABLE employee (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username VARCHAR(50) UNIQUE NOT NULL,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'service_type_enum') THEN
+        CREATE TYPE service_type_enum AS ENUM ('Construction', 'Delivery', 'Manufacture');
+    END IF;
 
-CREATE TYPE organization_type AS ENUM (
-    'IE',
-    'LLC',
-    'JSC'
-);
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'bid_status_enum') THEN
+        CREATE TYPE bid_status_enum AS ENUM ('Created', 'Published', 'Canceled');
+    END IF;
 
-CREATE TABLE organization (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    type organization_type,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'author_type_enum') THEN
+        CREATE TYPE author_type_enum AS ENUM ('Organization', 'User');
+    END IF;
 
-CREATE TABLE organization_responsible (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    organization_id UUID REFERENCES organization(id) ON DELETE CASCADE,
-    user_id UUID REFERENCES employee(id) ON DELETE CASCADE
-);
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'decision_enum') THEN
+        CREATE TYPE decision_enum AS ENUM ('Approved', 'Rejected');
+    END IF;
+END $CREATION$;
 
-
-INSERT INTO employee (id, username, first_name, last_name, created_at, updated_at)
-VALUES ('bff35258-2e34-4458-8445-e8bbbc4d1c71', 'testuser1', 'Алексей', 'Алексеев', '2024-09-11T19:35:04+03:00', '2024-09-11T19:35:04+03:00'),
-       ('2064757a-28b9-4ae4-aec9-d8a1f5c39ba0', 'testuser2', 'Павел', 'Павлов', '2024-09-11T19:49:02+03:00', '2024-09-11T19:49:02+03:00');
-
-INSERT INTO organization (id, name, description, type, created_at, updated_at)
-VALUES ('f7586a5b-de63-4c20-990c-a6bec950ec56', 'organization1', 'description1', 'LLC', '2024-09-11T19:41:44+03:00', '2024-09-11T19:41:44+03:00');
-
-INSERT INTO organization_responsible (organization_id, user_id)
-VALUES ('f7586a5b-de63-4c20-990c-a6bec950ec56', 'bff35258-2e34-4458-8445-e8bbbc4d1c71');
-
-
-CREATE TYPE tender_status_enum AS ENUM ('Created', 'Published', 'Closed');
-
-CREATE TABLE tender (
+-- tender - это тендер
+CREATE TABLE IF NOT EXISTS tender (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     status tender_status_enum NOT NULL,
     current_version INTEGER NOT NULL,
@@ -53,9 +31,8 @@ CREATE TABLE tender (
     organization_id UUID NOT NULL REFERENCES organization(id) ON DELETE CASCADE
 );
 
-CREATE TYPE service_type_enum AS ENUM ('Construction', 'Delivery', 'Manufacture');
-
-CREATE TABLE tender_versions (
+-- tender_versions - это версии тендеров
+CREATE TABLE IF NOT EXISTS tender_versions (
     id SERIAL PRIMARY KEY,
     version INTEGER NOT NULL,
     name VARCHAR(100) NOT NULL,
@@ -65,33 +42,29 @@ CREATE TABLE tender_versions (
     UNIQUE (tender_id, version)
 );
 
-CREATE TYPE bid_status_enum AS ENUM ('Created', 'Published', 'Canceled');
-
-CREATE TYPE author_type_enum AS ENUM ('Organization', 'User');
-
-CREATE TABLE bid (
+-- bid - это предложение для тендера
+CREATE TABLE IF NOT EXISTS bid (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     status bid_status_enum NOT NULL,
     tender_id UUID UNIQUE NOT NULL REFERENCES tender(id) ON DELETE CASCADE,
     author_type author_type_enum NOT NULL,
     author_id UUID NOT NULL,
     current_version INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE bid_versions (
+-- bid_versions - это версии предложений для тендеров
+CREATE TABLE IF NOT EXISTS bid_versions (
     id SERIAL PRIMARY KEY,
-    version INTEGER PRIMARY KEY,
+    version INTEGER NOT NULL,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(500) NOT NULL,
     bid_id UUID REFERENCES bid(id) ON DELETE CASCADE,
     UNIQUE (bid_id, version)
 );
 
-
-CREATE TYPE decision_enum AS ENUM ('Approved', 'Rejected');
-
-CREATE bid_decisions (
+-- bid_decisions - решения по предложениям
+CREATE TABLE IF NOT EXISTS bid_decisions (
     id SERIAL PRIMARY KEY,
     decision decision_enum,
     organization_id UUID NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
@@ -99,8 +72,8 @@ CREATE bid_decisions (
     bid_id UUID REFERENCES bid(id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE bid_review (
+-- bid_review - отзыв по предложению
+CREATE TABLE IF NOT EXISTS bid_review (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     description VARCHAR(1000) NOT NULL,
     organization_id UUID NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
--- internal/datastore/connect_db.go
+++ internal/datastore/connect_db.go
@@ -32,10 +32,30 @@ func CreateNewDB() (*sql.DB, error) {
 		return nil, fmt.Errorf("переменная окружения database не установлена")
 	}
 
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, database)
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", username, password, host, port, database)
 	dtb, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, fmt.Errorf("ошибка открытия соединения с базой данных: %v", err)
 	}
+
+	err = setup(dtb)
+	if err != nil {
+		return nil, err
+	}
 	return dtb, nil
 }
+
+// setup создает необходимые таблицы
+func setup(dtb *sql.DB) error {
+	sqlScript, err := os.ReadFile("init.sql")
+	if err != nil {
+		return fmt.Errorf("ошибка чтения файла SQL-скрипта: %v", err)
+	}
+
+	_, err = dtb.Exec(string(sqlScript))
+	if err != nil {
+		return fmt.Errorf("ошибка выполнения SQL-скрипта: %v", err)
+	}
+	return nil
+}
+
